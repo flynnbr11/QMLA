3472a8b2b6c924a748c1b1e5b97bdc7e7d7d30f9
Brian Flynn <bf16951@newblue2.cm.cluster>
2018-04-13 16:10:21 +0100

    Script launching workers on separate nodes on BC

--- ParallelDevelopment/launch_qmd_parallel.sh
+++ ParallelDevelopment/launch_qmd_parallel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#PBS -l nodes=1:ppn=4,walltime=00:30:00
-#PBS -q testq
+#PBS -l nodes=2:ppn=3, walltime=06:00:00
+#PBS -q veryshort
 
 host=$(hostname)
 echo "host= $host"
@@ -36,7 +36,7 @@ then
     lib_dir="/panfs/panasas01/phys/bf16951/QMD/Libraries/QML_lib"
     script_dir="/panfs/panasas01/phys/bf16951/QMD/ExperimentalSimulations"
     module load tools/redis-4.0.8
-    module load mvapich/gcc/64/1.2.0-qlc
+    module load languages/intel-compiler-16-u2
     SERVER_HOST=$(head -1 "$PBS_NODEFILE")
     echo "launching redis"
     redis-server --protected-mode no &
@@ -55,13 +55,13 @@ echo "REDIS_URL is $REDIS_URL"
 
 cd $lib_dir
 
-# mpirun -np 1 -ppn 4 rq worker -u $REDIS_URL > logs/worker_$HOSTNAME.log 2>&1 &
+mpirun -np 6 rq worker -u $REDIS_URL > logs/worker_$HOSTNAME.log 2>&1 &
 echo "launching rq worker"
-rq worker -u $REDIS_URL > logs/worker_$HOSTNAME.log 2>&1 &
-
+#rq worker -u $REDIS_URL > logs/worker_$HOSTNAME.log 2>&1 &
+sleep 4
 
 cd $script_dir
-python3 Exp.py -rq=0 -p=15 -e=5 -bt=2 -host=$SERVER_HOST
+python3 Exp.py -rq=1 -p=150 -e=50 -bt=25 -host=$SERVER_HOST
 
 
 
