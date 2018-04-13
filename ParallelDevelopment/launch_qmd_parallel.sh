26cc4800c2d513df7132889ab936944ac259d23a
Brian Flynn <flynnbr@tcd.ie>
2018-04-13 11:32:31 +0100

    bash script launches locally

--- ParallelDevelopment/launch_qmd_parallel.sh
+++ ParallelDevelopment/launch_qmd_parallel.sh
@@ -12,6 +12,7 @@ then
     lib_dir="/home/bf16951/Dropbox/QML_share_stateofart/QMD/Libraries/QML_lib"
     script_dir="/home/bf16951/Dropbox/QML_share_stateofart/QMD/ExperimentalSimulations"
     SERVER_HOST='localhost'
+    ~/redis-4.0.8/src/redis-server & 
         
 elif [ "$host" == "newblue4" ]
 then
@@ -22,7 +23,8 @@ then
     script_dir="/panfs/panasas01/phys/bf16951/QMD/ExperimentalSimulations"
     module load tools/redis-4.0.8
     module load mvapich/gcc/64/1.2.0-qlc
-#    SERVER_HOST=$(head -1 "$PBS_NODEFILE")
+    echo "launching redis"
+    redis-server --protected-mode no &
     SERVER_HOST='localhost'
 
 
@@ -36,11 +38,12 @@ then
     module load tools/redis-4.0.8
     module load mvapich/gcc/64/1.2.0-qlc
     SERVER_HOST=$(head -1 "$PBS_NODEFILE")
-#    SERVER_HOST='localhost'
+    echo "launching redis"
+    redis-server --protected-mode no &
 
 
 else
-    echo "neither local machine or blue crystal identified." 
+    echo "Neither local machine (Brian's university laptop) or blue crystal identified." 
 fi
 
 set -x
@@ -51,8 +54,6 @@ echo "REDIS_URL is $REDIS_URL"
 #TODO create a redis config
 
 cd $lib_dir
-echo "launching redis"
-redis-server --protected-mode no &
 
 # mpirun -np 1 -ppn 4 rq worker -u $REDIS_URL > logs/worker_$HOSTNAME.log 2>&1 &
 echo "launching rq worker"
@@ -60,7 +61,7 @@ rq worker -u $REDIS_URL > logs/worker_$HOSTNAME.log 2>&1 &
 
 
 cd $script_dir
-python3 Exp.py -rq=1 -p=15 -e=5 -bt=2 -host=$SERVER_HOST
+python3 Exp.py -rq=0 -p=15 -e=5 -bt=2 -host=$SERVER_HOST
 
 
 
---
+++ ParallelDevelopment/try_redis.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+exec bash 
+redis-server
