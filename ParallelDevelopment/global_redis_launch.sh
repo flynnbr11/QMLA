4944669c71f332ad73e225c24870d36cf962a1d2
Brian Flynn <bf16951@newblue2.cm.cluster>
2018-05-04 15:29:30 +0100

    Using redis port numbers to keep instances of qmd on same node separate

--- ParallelDevelopment/global_redis_launch.sh
+++ ParallelDevelopment/global_redis_launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "local host is $(hostname)" 
+echo "GLobal host is $(hostname)" 
 
 module load tools/redis-4.0.8
 redis-cli flushall
--- ParallelDevelopment/test_launch_script.sh
+++ ParallelDevelopment/test_launch_script.sh
@@ -5,7 +5,7 @@ echo "local host is $(hostname). Global redis launced here."
 
 global_server=$(hostname)
 
-for i in `seq 1 1`;
+for i in `seq 1 40`;
 do
 	qsub -v QMD_ID=$i,GLOBAL_SERVER=$global_server  launch_qmd_parallel.sh
 done 
