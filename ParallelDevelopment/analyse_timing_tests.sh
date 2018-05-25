3fdd5994e541be6590b609a8dee6dad737db1005
Brian Flynn <bf16951@newblue4.cm.cluster>
2018-05-25 10:03:19 +0100

    resampling params not passing through QMD

--- ParallelDevelopment/analyse_timing_tests.sh
+++ ParallelDevelopment/analyse_timing_tests.sh
@@ -1,5 +1,5 @@
 
 #!/bin/bash 
 cd ../Libraries/QML_lib
-python3 AnalyseMultipleQMD.py -dir=/panfs/panasas01/phys/bf16951/QMD/ExperimentalSimulations/Results/May_24/15_48
+python3 AnalyseMultipleQMD.py -dir=/panfs/panasas01/phys/bf16951/QMD/ExperimentalSimulations/Results/May_24/17_06
 
