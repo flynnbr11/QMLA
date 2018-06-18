dede31773ba400c518a8d6cffa7fa96e82eb5302
Brian Flynn <flynnbr@tcd.ie>
2018-06-18 11:54:49 +0100

    Updated tree plots for multiple QMD to encode number of wins in node colour

--- ExperimentalSimulations/qmd_pipeline.sh
+++ ExperimentalSimulations/qmd_pipeline.sh
@@ -8,18 +8,21 @@ long_dir="$cwd/Results/$directory"
 bcsv="cumulative.csv"
 bayes_csv="$long_dir$bcsv"
 
+this_log='graph_dev.log'
+
 rm -r $long_dir
+rm $this_log
 mkdir -p $long_dir
 
 q_id=0
-for i in `seq 1 10`;
+for i in `seq 1 2`;
 do
     for j in `seq 1 1`;
     do
         let num_prt="$i+10"
         redis-cli flushall
         let q_id="$q_id+1"
-        python3 Exp.py -p 10 -e 4 -rq=0 -dir=$directory -qid=$q_id -pt=1 -pkl=1 -log='highTfailure.log' -cb=$bayes_csv
+        python3 Exp.py -p 5 -e 2 -rq=0 -dir=$directory -qid=$q_id -pt=1 -pkl=1 -log=$this_log -cb=$bayes_csv
     done 
 done
 
