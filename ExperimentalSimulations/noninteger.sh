2b7fa4f4b431865e75daf6a931c0c0dc5774d81b
Brian Flynn <flynnbr@tcd.ie>
2018-06-26 15:18:55 +0100

    Removed old/unneeded files from ExperimentalSimulation folder

--- ExperimentalSimulations/noninteger.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in $(seq 0 0.1 1);
-do
-    echo $i
-done
--- ExperimentalSimulations/pid_rq_test.py
+++
@@ -1,8 +0,0 @@
-
-import pickle
-pickle.HIGHEST_PROTOCOL=2
-
-def print_pid_retest():
-#    import os
-#    print("My pid is:", os.getpid())
-    print("TEST")
