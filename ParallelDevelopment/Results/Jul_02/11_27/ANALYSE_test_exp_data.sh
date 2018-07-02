f2df153f0519f0ccbfc0f17b2034b0376631fc63
Brian Flynn <bf16951@newblue4.cm.cluster>
2018-07-02 11:30:29 +0100

    Log configurations of QMD runs on BC

---
+++ ParallelDevelopment/Results/Jul_02/11_27/ANALYSE_test_exp_data.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash 
+cd /panfs/panasas01/phys/bf16951/QMD/Libraries/QML_lib
+python3 AnalyseMultipleQMD.py -dir=/panfs/panasas01/phys/bf16951/QMD/ParallelDevelopment/Results/Jul_02/11_27 --bayes_csv=/panfs/panasas01/phys/bf16951/QMD/ParallelDevelopment/Results/Jul_02/11_27/multiQMD.csv
+
---
+++ ParallelDevelopment/Results/Jul_02/11_29/ANALYSE_test_exp_data.sh
@@ -0,0 +1,5 @@
+
+#!/bin/bash 
+cd /panfs/panasas01/phys/bf16951/QMD/Libraries/QML_lib
+python3 AnalyseMultipleQMD.py -dir=/panfs/panasas01/phys/bf16951/QMD/ParallelDevelopment/Results/Jul_02/11_29 --bayes_csv=/panfs/panasas01/phys/bf16951/QMD/ParallelDevelopment/Results/Jul_02/11_29/multiQMD.csv
+
