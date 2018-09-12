86de71c5c3fba281f53fc68dc48f4a73c0a431bf
Brian Flynn <flynnbr@tcd.ie>
2018-09-12 19:18:29 +0100

    Changed priors to broad

--- ExperimentalSimulations/Exp.py
+++ ExperimentalSimulations/Exp.py
@@ -136,28 +136,17 @@ log_print(
 if global_variables.custom_prior:
 
     prior_specific_terms = {
+
         'xTy' : [1.0, 0.5],
         'xTz' : [1.0, 0.5],
         'yTz' : [1.0, 0.5],
         # Values below correspond to simulated data
-        'xTx' : [-0.04, 0.01], # true value 2.7
-        'yTy' : [0.03, 0.01], # true value 2.7
-        'zTz' : [-1.94, 0.01], # true value 2.14
-        'xTi' : [0.17, 0.01], # TODO Broaden, testing with small dist
-        'yTi' : [0.77, 0.01],
-        'zTi' : [-0.08, 0.01],
-
-
-        # 'xTy' : [1.0, 0.5],
-        # 'xTz' : [1.0, 0.5],
-        # 'yTz' : [1.0, 0.5],
-        # # Values below correspond to simulated data
-        # 'xTx' : [-3.0, 0.3], # true value 2.7
-        # 'yTy' : [-3.0, 0.3], # true value 2.7
-        # 'zTz' : [-2.1, 0.3], # true value 2.14
-        # 'xTi' : [1.0, 0.5], # TODO Broaden, testing with small dist
-        # 'yTi' : [1.0, 0.5],
-        # 'zTi' : [1.0, 0.5],
+        'xTx' : [-1.5, 1.0], # true value 2.7
+        'yTy' : [-1.5, 1.0], # true value 2.7
+        'zTz' : [-1.5, 1.0], # true value 2.14
+        'xTi' : [1.5, 1.0], # TODO Broaden, testing with small dist
+        'yTi' : [1.5, 1.0],
+        'zTi' : [1.5, 1.0],
 
         # Values below correspond to Andreas' inital QMD values for this data set
 #        'xTx' : [-1.59329862 , 0.19606237], # true value 2.7
