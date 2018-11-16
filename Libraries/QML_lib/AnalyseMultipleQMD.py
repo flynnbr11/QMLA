a580aca6de5ef84d69226fbd34944ddceb196507
Brian Flynn <flynnbr@tcd.ie>
2018-11-16 15:39:47 +0000

    made true model bar green in model scores plot

--- Libraries/QML_lib/AnalyseMultipleQMD.py
+++ Libraries/QML_lib/AnalyseMultipleQMD.py
@@ -1025,7 +1025,8 @@ def plot_scores(
         colours[true_idx] = 'green'
     except:
         print("True model", latex_true_op, "not in ", latex_model_names)
-        pass
+        raise
+        # pass
 
 
     ax.barh(ind, scores, width, color="blue")
