6b3ac5e2e7a9e7ce97cbd3ad1b782f228ca7c2ba
Brian Flynn <flynnbr@tcd.ie>
2019-10-02 18:15:45 +0100

    set alternative growth rules to match required runs

--- Launch/local_launch.sh
+++ Launch/local_launch.sh
@@ -10,8 +10,8 @@ num_tests=1
 qhl_test=0 # don't perform QMLA; perform QHL on known correct model
 multiple_qhl=0 # perform QHL for defined list of models.
 do_further_qhl=0 # QHL refinement to best performing models 
-exp_data=0
-simulate_experiment=1
+exp_data=1
+simulate_experiment=0
 q_id=0 # can start from other ID if desired
 
 ### ---------------------------------------------------###
@@ -85,10 +85,10 @@ git_commit=$(git rev-parse HEAD)
 # sim_growth_rule='nearest_neighbour_pauli_2D'
 # sim_growth_rule='hopping_probabilistic'
 # sim_growth_rule='hopping_predetermined'
-sim_growth_rule='ising_probabilistic'
+# sim_growth_rule='ising_probabilistic'
 # sim_growth_rule='ising_predetermined'
 # sim_growth_rule='heisenberg_xyz_probabilistic'
-# sim_growth_rule='heisenberg_xyz_predetermined'
+sim_growth_rule='heisenberg_xyz_predetermined'
 # sim_growth_rule='example'
 
 
@@ -97,13 +97,14 @@ sim_growth_rule='ising_probabilistic'
 
 # exp_growth_rule='two_qubit_ising_rotation_hyperfine_transverse'
 # exp_growth_rule='NV_alternative_model'
+exp_growth_rule='NV_alternative_model_2'
 # exp_growth_rule='nv_experiment_vary_model'
 # exp_growth_rule='NV_centre_revivals'
 # exp_growth_rule='two_qubit_ising_rotation_hyperfine'
 # exp_growth_rule='NV_centre_spin_large_bath'
 # exp_growth_rule='NV_spin_full_access'
 # exp_growth_rule='NV_centre_experiment_debug'
-exp_growth_rule='reduced_nv_experiment'
+# exp_growth_rule='reduced_nv_experiment'
 # exp_growth_rule='NV_fitness_growth'
 
 
--- Libraries/GrowthRuleClasses/NVCentreAlternativeTrueModel.py
+++ Libraries/GrowthRuleClasses/NVCentreAlternativeTrueModel.py
@@ -23,11 +23,24 @@ class nv_centre_spin_experimental_method_alternative_true_model(
             **kwargs
         )
 
-        # self.true_operator = 'xTiPPxTxPPxTyPPyTiPPyTyPPzTiPPzTz'
-        # self.probe_generation_function = ProbeGeneration.NV_centre_ising_probes_plus
-        self.probe_generation_function = ProbeGeneration.separable_probe_dict
-        self.shared_probes = True
 
+        self.true_operator = 'xTiPPxTxPPyTiPPyTyPPzTiPPzTz'
+        # self.probe_generation_function = ProbeGeneration.NV_centre_ising_probes_plus
+        # self.probe_generation_function = ProbeGeneration.separable_probe_dict
+        # self.shared_probes = True
+
+
+        if self.true_operator == 'xTiPPxTxPPyTiPPyTyPPzTiPPzTz':
+            self.true_params = {
+                # Decohering param set
+                # From 3000exp/20000prt, BC SelectedRuns/Nov_28/15_14/results_049
+                'xTi': -0.98288958683093952, # -0.098288958683093952
+                'xTx': 6.7232235286284681, # 0.67232235286284681,  
+                'yTi': 6.4842202054983122,  # 0.64842202054983122, # 
+                'yTy': 2.7377867056770397,  # 0.27377867056770397, 
+                'zTi': 0.96477790489201143, # 0.096477790489201143, 
+                'zTz': 1.6034234519563935, #0.16034234519563935,
+            }
 
 
 class nv_centre_spin_experimental_method_alternative_true_model_second(
@@ -48,8 +61,9 @@ class nv_centre_spin_experimental_method_alternative_true_model_second(
         )
 
         self.true_operator = 'xTiPPxTxPPxTyPPyTiPPyTyPPzTiPPzTz'
-        # self.probe_generation_function = ProbeGeneration.NV_centre_ising_probes_plus
-        self.probe_generation_function = ProbeGeneration.separable_probe_dict
+        self.probe_generation_function = ProbeGeneration.NV_centre_ising_probes_plus
+        self.simulator_probe_generation_function = self.probe_generation_function # unless specifically different set of probes required
+        # self.probe_generation_function = ProbeGeneration.separable_probe_dict
 
 
 
