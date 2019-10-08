ba0928eff7960d29946d98322df82371f5ab827e
Brian Flynn <flynnbr@tcd.ie>
2019-10-08 14:58:29 +0100

    slight change

--- Launch/combine_results.py
+++ Launch/combine_results.py
@@ -4,10 +4,17 @@ import pandas as pd
 import pickle
 import datetime
 import os
+import sys
+sys.path.append(
+	os.path.join("..", "Libraries","QML_lib")
+)
+import GrowthRules
 
 results_directory = str(
 	os.getcwd() + '/Results/'
 )
+
+
 use_all_directories = False
 all_directories = {
 	'sim_1' : 'Oct_07/17_09',
@@ -27,16 +34,16 @@ if use_all_directories == True:
 	directories_to_use = list(all_directories.keys())
 else:
 	directories_to_use = [
-		# 'sim_1', 'sim_2' # for testing
-		'experimental_data_plusphase',
-		'experimental_data_plusrandom',
-		'simulation_plusphase',
-		'simulation_extended_true_model_plusphase',
-		'vary_model_3_params',
-		'vary_model_4_params',
-		'vary_model_5_params',
-		'vary_model_6_params',
-		'vary_model_7_params',
+		'sim_1', 'sim_2' # for testing
+		# 'experimental_data_plusphase',
+		# 'experimental_data_plusrandom',
+		# 'simulation_plusphase',
+		# 'simulation_extended_true_model_plusphase',
+		# 'vary_model_3_params',
+		# 'vary_model_4_params',
+		# 'vary_model_5_params',
+		# 'vary_model_6_params',
+		# 'vary_model_7_params',
 	]
 
 directories = [all_directories[d] for d in directories_to_use]
@@ -48,24 +55,46 @@ flatten = lambda l: [item for sublist in l for item in sublist]
 all_results_paths = flatten(all_results_paths)
 
 results_df = pd.DataFrame()
+directory_df = pd.DataFrame()
+
 
 for d in directories:
 	idx = directories.index(d)
-	d += '/'
-	description = directories_to_use[idx]
-	results_paths = glob.glob(d+'results*')
+	
 	true_params_path = str(d + 'true_params.p')
 	true_params = pickle.load(open(true_params_path, 'rb'))
-	true_op = true_params['true_op']
+
 	growth_gen = true_params['growth_generator']
+	growth_class = GrowthRules.get_growth_generator_class(
+		growth_generation_rule = growth_gen
+	)
+
+	directory_info = {
+		'Run' : directories_to_use[idx],
+		'RunIdx' : idx, 
+		'ResultsDirectory' : d,
+		'TrueModel' : true_params['true_op'],
+		'TrueParams' : true_params['params_dict'],
+		'GrowthGenerator' : growth_gen,
+		'TrueModelLatex' : growth_class.latex_name(
+			true_params['true_op']
+		)
+	}
+
+	d += '/'
+	directory_data = pd.Series(directory_info)
+	directory_df = directory_df.append(
+		directory_data, 
+		ignore_index = True
+	)
+
+	results_paths = glob.glob(d+'results*')
 
 	for results_file in results_paths:
 	    res = pickle.load(open(results_file, 'rb'))
 	    data = pd.Series(res)
-	    data['TrueModel'] = true_op
-	    data['GrowthGenerator'] = growth_gen
 	    data['ResultsDirectory'] = d
-	    data['Run'] = description
+	    data['RunIdx'] = idx
 	    results_df = results_df.append(
 	        data, 
 	        ignore_index=True
@@ -91,6 +120,7 @@ day = now.strftime("%d")
 
 results_directory = "SharedResults/{}_{}/{}_{}".format(month, day, hour, minute)
 results_file = "{}/combined_results.csv".format(results_directory)
+run_info_file = "{}/run_info.csv".format(results_directory)
 
 if not os.path.exists(results_directory):
 	os.makedirs(results_directory)
@@ -98,5 +128,9 @@ if not os.path.exists(results_directory):
 print("Storing results dataframe in {}".format(results_file))
 results_df.to_csv(
 	results_file
+)
+
+directory_df.to_csv(
+	run_info_file
 )
 
