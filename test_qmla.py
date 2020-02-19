11ba430c09b1092d83f1ee6808dfc42ab3dd33f8
Brian Flynn <flynnbr@tcd.ie>
2020-02-19 18:16:39 +0000

    testing test infrastructure

--- test_qmla.py
+++ test_qmla.py
@@ -4,14 +4,15 @@ import qmla
 
 
 def test_qmla_import():
-    growth = qmla.growth_rules.HeisenbergXYZProbabilistic(
+    growth_rule = qmla.growth_rules.HeisenbergXYZProbabilistic(
         growth_generation_rule = 'HeisenbergXYZProbabilistic'
     )
     assert \
-        growth.lattice_dimension \
+        growth_rule.lattice_dimension \
         == 1, \
         "Testing GR lattice dimension" 
 
+    assert 1 == 1
 
 
 # def test_qmla_growth_rule():
