5811ee979e29c65fb8ea2504a575edfbbdfb744e
Brian Flynn <flynnbr@tcd.ie>
2020-02-21 13:31:37 +0000

    increased num probes for subsystem test

--- qmla/growth_rules/heisenberg_xyz.py
+++ qmla/growth_rules/heisenberg_xyz.py
@@ -54,7 +54,7 @@ class HeisenbergXYZProbabilistic(
             # 'y',
             'z'
         ]
-        self.num_probes = 5
+        self.num_probes = 50
         self.max_time_to_consider = 20
         # fitness calculation parameters. fitness calculation inherited.
         # 'all' # 'all' # at each generation Badassness parameter
