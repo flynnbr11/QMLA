25615102eb1ac7d96c9d880547813d942f57a622
Brian Flynn <flynnbr@tcd.ie>
2020-03-11 19:26:51 +0000

    set up for GA test using ELO ratings as fitness

--- qmla/growth_rules/genetic.py
+++ qmla/growth_rules/genetic.py
@@ -88,11 +88,11 @@ class Genetic(
         # self.true_model = 'pauliSet_xJx_1J2_d3+pauliSet_yJy_1J2_d3'
         self.max_num_probe_qubits = self.num_sites
         # test
-        self.max_spawn_depth = 1
-        self.initial_num_models = 8
+        # self.max_spawn_depth = 1
+        # self.initial_num_models = 8
         # self.tree_completed_initially = True
-        # self.max_spawn_depth = 50
-        # self.initial_num_models = 20
+        self.max_spawn_depth = 20
+        self.initial_num_models = 16
         self.initial_models = self.genetic_algorithm.random_initial_models(
             num_models=self.initial_num_models
         )
