76af0d0090a80d37004948b592f527ea89920557
Brian Flynn <flynnbr@tcd.ie>
2020-03-12 15:37:03 +0000

    error catching

--- qmla/analysis/analysis_and_plot_functions.py
+++ qmla/analysis/analysis_and_plot_functions.py
@@ -4157,6 +4157,7 @@ def model_generation_probability(
             print("Failed to draw {} samples from {} dist".format(num_samples, 2**num_terms))
             print("avg num mods per inst:", avg_num_mods_per_instance)
             print("std mods per inst:", std_dev_num_mods_per_instance)
+            raise
         for m in model_ids: 
             counts[m] += 1
     counts /= num_trials
