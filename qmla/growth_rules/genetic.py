d2958471e8ab67d4424631b2ccbf71dd28b851b3
Brian Flynn <flynnbr@tcd.ie>
2020-03-13 15:55:32 +0000

    for BC test

--- qmla/growth_rules/genetic.py
+++ qmla/growth_rules/genetic.py
@@ -94,12 +94,11 @@ class Genetic(
         # self.true_model = 'pauliSet_xJx_1J2_d3+pauliSet_yJy_1J2_d3'
         self.max_num_probe_qubits = self.num_sites
         # test
-        self.max_spawn_depth = 2
-        self.initial_num_models = 8
-        # self.tree_completed_initially = True
+        # self.max_spawn_depth = 2
+        # self.initial_num_models = 8
         # default test - 32 generations x 16 starters
-        # self.max_spawn_depth = 32
-        # self.initial_num_models = 16
+        self.max_spawn_depth = 32
+        self.initial_num_models = 16
         self.initial_models = self.genetic_algorithm.random_initial_models(
             num_models=self.initial_num_models
         )
