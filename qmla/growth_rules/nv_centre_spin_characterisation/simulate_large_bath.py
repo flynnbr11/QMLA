e8885f562e016216930516c8cf3186644610d29a
Brian Flynn <flynnbr@tcd.ie>
2020-05-08 19:08:33 +0100

    3 terms

--- qmla/growth_rules/nv_centre_spin_characterisation/simulate_large_bath.py
+++ qmla/growth_rules/nv_centre_spin_characterisation/simulate_large_bath.py
@@ -42,8 +42,8 @@ class SimulatedNVCentre(
             # 'pauliSet_1_z_d1' : 4, # TEST - kHz
             # spin
             'pauliSet_1_x_d3' : B*g*bohr_magneton/hbar, # ~1.943 GHz
-            'pauliSet_1_y_d3' : B*g*bohr_magneton/hbar,
-            'pauliSet_1_z_d3' : B*g*bohr_magneton/hbar,
+            # 'pauliSet_1_y_d3' : B*g*bohr_magneton/hbar,
+            # 'pauliSet_1_z_d3' : B*g*bohr_magneton/hbar,
             # nitrogen nuclei
             # 'pauliSet_2_x_d3' : B*gamma_n , # ~3.37GHz
             # 'pauliSet_2_y_d3' : B*gamma_n ,
@@ -53,12 +53,12 @@ class SimulatedNVCentre(
             # 'pauliSet_3_y_d3' : B * gamma ,
             # 'pauliSet_3_z_d3' : B * gamma ,
             # # interactions: spin with nitrogen nuclei
-            'pauliSet_1J2_xJx_d3' : 2.7e6, # 2.7MHz
-            'pauliSet_1J2_yJy_d3' : 2.7e6,
+            # 'pauliSet_1J2_xJx_d3' : 2.7e6, # 2.7MHz
+            # 'pauliSet_1J2_yJy_d3' : 2.7e6,
             'pauliSet_1J2_zJz_d3' : 2.14e6,
             # # interactions: spin with carbon nuclei
-            'pauliSet_1J3_xJx_d3' : 2.4e6, # 2.4MHz
-            'pauliSet_1J3_yJy_d3' : 2.4e6, 
+            # 'pauliSet_1J3_xJx_d3' : 2.4e6, # 2.4MHz
+            # 'pauliSet_1J3_yJy_d3' : 2.4e6, 
             'pauliSet_1J3_zJz_d3' : 2.4e6,
         }
         
