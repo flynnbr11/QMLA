d5497709473246cab202be650f257367a68b1e18
Brian Flynn <flynnbr@tcd.ie>
2020-05-23 10:13:06 +0100

    very thin prior for GA

--- Launch/local_launch.sh
+++ Launch/local_launch.sh
@@ -7,7 +7,7 @@ printf "$day_time: \t $test_description \n" >> QMD_Results_directories.log
 # Running QMD essentials
 ### ---------------------------------------------------###
 num_tests=1
-qhl_test=0 # don't perform QMLA; perform QHL on known correct model
+qhl_test=1 # don't perform QMLA; perform QHL on known correct model
 multiple_qhl=0 # perform QHL for defined list of models.
 do_further_qhl=0 # QHL refinement to best performing models 
 exp_data=0
