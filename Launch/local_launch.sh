7f86f52f5b79db652c1b8c8b41469cd72c0e946d
Brian Flynn <flynnbr@tcd.ie>
2020-06-01 23:01:44 +0100

    chebyshev distance in heuristic

--- Launch/local_launch.sh
+++ Launch/local_launch.sh
@@ -19,8 +19,8 @@ q_id=0 # can start from other ID if desired
 ### ---------------------------------------------------###
 # QHL parameters
 ### --------------------------------------------------###
-exp=200
-prt=1000
+exp=20
+prt=100
 pgh=1.0
 pgh_exponent=1.0
 pgh_increase=0 # whether to add to time found by PGH (bool)
--- qmla/shared_functionality/experiment_design_heuristics.py
+++ qmla/shared_functionality/experiment_design_heuristics.py
@@ -63,7 +63,7 @@ class MultiParticleGuessHeuristic(qi.Heuristic):
             'canberra', 'braycurtis','minkowski', 
         ]
         self.designed_times = { m : {} for m in distance_metrics }
-        self.distance_metric_to_use = 'euclidean'
+        self.distance_metric_to_use = 'chebyshev'
 
     def __call__(
         self,
