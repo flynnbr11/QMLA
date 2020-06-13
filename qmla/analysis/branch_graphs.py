0049cffebde4c24ef2fe57a2336a8feae29d78cf
Brian Flynn <flynnbr@tcd.ie>
2020-06-13 15:45:15 +0100

    using complete cmap for f score

--- qmla/analysis/branch_graphs.py
+++ qmla/analysis/branch_graphs.py
@@ -67,7 +67,7 @@ def plot_qmla_branches(q, return_graphs=False):
     # f_score_cmap = plt.cm.get_cmap('Blues')
     # f_score_cmap = qmla.utilities.truncate_colormap(f_score_cmap, 0.25, 1.0)
     f_score_cmap = plt.cm.get_cmap('tab20c_r')
-    f_score_cmap = qmla.utilities.truncate_colormap(f_score_cmap, 0.6, 1.0)
+    # f_score_cmap = qmla.utilities.truncate_colormap(f_score_cmap, 0.6, 1.0)
 
     bf_cmap = plt.cm.get_cmap('PRGn')
     bf_cmap = qmla.utilities.truncate_colormap(bf_cmap, 0.05, 0.95)
