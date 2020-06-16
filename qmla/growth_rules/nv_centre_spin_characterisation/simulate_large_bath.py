2a54c6bd30f985bdfa5c3cccbf810ce205fdbc76
Brian Flynn <flynnbr@tcd.ie>
2020-06-16 21:47:54 +0100

    IQLE mode for BF matrix with F scores for GA

--- qmla/growth_rules/nv_centre_spin_characterisation/simulate_large_bath.py
+++ qmla/growth_rules/nv_centre_spin_characterisation/simulate_large_bath.py
@@ -289,8 +289,10 @@ class TestSimulatedNVCentre(
             # 'pauliSet_1_y_d1' : 3.9431238095238094 * (10**order_mag),
             # 'pauliSet_1_z_d1' : 5.9431238095238094 * (10**order_mag),
 
-            'pauliSet_1_x_d2' : 1.9431238095238094 * (10**order_mag),
-            'pauliSet_1_y_d2' : 8.9431238095238094 * (10**order_mag),
+            'pauliSet_1_x_d2' : 3.2431238095238094 * (10**order_mag),
+            'pauliSet_1_y_d2' : 6.9431238095238094 * (10**order_mag),
+            # 'pauliSet_1_x_d2' : 1.9431238095238094 * (10**order_mag),
+            # 'pauliSet_1_y_d2' : 8.9431238095238094 * (10**order_mag),
             # 'pauliSet_1_z_d2' : 3.9431238095238094 * (10**order_mag),
             # 'pauliSet_2_x_d2' : 4.9431238095238094 * (10**order_mag),
             # 'pauliSet_2_y_d2' : 7.9431238095238094 * (10**order_mag),
