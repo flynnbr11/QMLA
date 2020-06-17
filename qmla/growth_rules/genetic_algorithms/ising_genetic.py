fcbda56a485af20aa9a8822249ab8d91b37a7bdc
Brian Flynn <flynnbr@tcd.ie>
2020-06-17 21:10:50 +0100

    single layer for BF mtx

--- qmla/growth_rules/genetic_algorithms/ising_genetic.py
+++ qmla/growth_rules/genetic_algorithms/ising_genetic.py
@@ -147,7 +147,7 @@ class IsingGeneticTest(
             # F = 1
             'pauliSet_1J2_zJz_d5+pauliSet_1J3_zJz_d5+pauliSet_2J3_zJz_d5+pauliSet_2J5_zJz_d5+pauliSet_3J5_zJz_d5' # F=1
         ]
-        self.initial_models = list(np.random.choice(test_fitness_models, 4, replace=False))
+        self.initial_models = list(np.random.choice(test_fitness_models, 9, replace=False))
         # self.initial_models = self.genetic_algorithm.random_initial_models(4)
 
         # if self.true_model not in self.initial_models:
