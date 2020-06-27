c9e933ea38e06d626c87af1917237c869c8e3ab6
Brian Flynn <flynnbr@tcd.ie>
2020-06-28 00:33:05 +0100

    lower time reqst

--- qmla/growth_rules/nv_centre_spin_characterisation/nv_centre_N_spin_bath.py
+++ qmla/growth_rules/nv_centre_spin_characterisation/nv_centre_N_spin_bath.py
@@ -116,7 +116,7 @@ class NVCentreNQubitBath(
             'other': 0
         }
         self.num_processes_to_parallelise_over = len(self.initial_models)
-        self.timing_insurance_factor = 0.3
+        self.timing_insurance_factor = 0.2
 
 
     # Model generation / QMLA progression
