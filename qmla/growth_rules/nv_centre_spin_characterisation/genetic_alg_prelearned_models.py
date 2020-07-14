10f407db505477742d2f103b9491cef26cfb6e49
Brian Flynn <flynnbr@tcd.ie>
2020-07-14 22:26:21 +0100

    single probe for evaluation

--- qmla/growth_rules/nv_centre_spin_characterisation/genetic_alg_prelearned_models.py
+++ qmla/growth_rules/nv_centre_spin_characterisation/genetic_alg_prelearned_models.py
@@ -62,8 +62,8 @@ class NVCentreGenticAlgorithmPrelearnedParameters(
         # Modular functions
         # self.latex_model_naming_function = qmla.shared_functionality.latex_model_names.nv_centre_SAT
         self.probe_generation_function = qmla.shared_functionality.probe_set_generation.plus_plus_with_phase_difference
-        # self.evaluation_probe_generation_function = qmla.shared_functionality.probe_set_generation.plus_plus_with_phase_difference
-        self.evaluation_probe_generation_function = qmla.shared_functionality.probe_set_generation.separable_probe_dict
+        self.evaluation_probe_generation_function = qmla.shared_functionality.probe_set_generation.plus_plus_with_phase_difference
+        # self.evaluation_probe_generation_function = qmla.shared_functionality.probe_set_generation.separable_probe_dict
         self.simulator_probe_generation_function = self.probe_generation_function
         self.shared_probes = True
         self.num_probes = 5
