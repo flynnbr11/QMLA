95984adc71f7b97be8a329d0f128d12ae8c390d2
Brian Flynn <flynnbr@tcd.ie>
2020-07-29 15:03:43 +0100

    5 qubits

--- qmla/shared_functionality/probe_transformer.py
+++ qmla/shared_functionality/probe_transformer.py
@@ -19,19 +19,20 @@ class ProbeTransformation():
 class FirstQuantisationToJordanWigner(ProbeTransformation):
     r"""
     
-    This class is used to map from first transformation 
+    This class is used to map from first quantisation 
     to an occupation basis which can be acted on by operators
     which have undergone the Jordan Wigner transformation. 
     
     e.g. a spin system has 
     :math:`
-    \| \psi^{(1)}\rangle = \alpha_{\downarrow} \| \downarrow \rangle + \alpha{\uparrow} \| \uparrow \rangle
-     = \alpha_{\downarrow} \| \downarrow \rangle + \alpha{\uparrow} \| \uparrow \rangle`
+    \| \psi^{(1)}\rangle 
+    = \alpha_{\downarrow} \| \downarrow \rangle + \alpha{\uparrow} \| \uparrow \rangle
+    = \alpha_{\downarrow} \| \downarrow \rangle + \alpha{\uparrow} \| \uparrow \rangle`
      
-     An equivelent representation which can be used by second quantised operators is
-     a binary encoding: the first element is whether a down spin is present; 
-     the second element is wehther an up spin is present:
-     :math:` \| \psi^{(2)}\rangle = \alpha_{\downarrow} \| 10 \rangle + \alpha{\uparrow} \| 01 \rangle`
+    An equivelent representation which can be used by second quantised operators is
+    a binary encoding: the first element is whether a down spin is present; 
+    the second element is wehther an up spin is present:
+    :math:` \| \psi^{(2)}\rangle = \alpha_{\downarrow} \| 10 \rangle + \alpha{\uparrow} \| 01 \rangle`
      
     We can therefore move between bases by mapping a probe :math:`\| \psi \rangle`, 
     from the first quantisation picture to the second (Jordan Wigner) picture, via
