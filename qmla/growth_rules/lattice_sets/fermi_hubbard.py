9b2cfdd30a7337d4eadde3c121f64c22c6ac59f7
Brian Flynn <flynnbr@tcd.ie>
2020-07-30 15:57:27 +0100

    using manually set probes for FH

--- qmla/growth_rules/lattice_sets/fermi_hubbard.py
+++ qmla/growth_rules/lattice_sets/fermi_hubbard.py
@@ -52,8 +52,8 @@ class FermiHubbardLatticeSet(
             # Default for FH
             
             self.probe_transformer = qmla.shared_functionality.probe_transformer.ProbeTransformation()        
-            # self.probe_generation_function = qmla.shared_functionality.probe_set_generation.test_probes_second_quantisation
-            self.probe_generation_function = qmla.shared_functionality.probe_set_generation.separable_fermi_hubbard_half_filled
+            self.probe_generation_function = qmla.shared_functionality.probe_set_generation.test_probes_second_quantisation
+            # self.probe_generation_function = qmla.shared_functionality.probe_set_generation.separable_fermi_hubbard_half_filled
 
         self.plot_probe_generation_function = qmla.shared_functionality.probe_set_generation.fermi_hubbard_occupation_basis_down_in_first_site
         # self.plot_probe_generation_function = qmla.shared_functionality.probe_set_generation.fermi_hubbard_half_filled_superposition
