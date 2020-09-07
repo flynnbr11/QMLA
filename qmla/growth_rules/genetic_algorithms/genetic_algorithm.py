7b54141b5df0b0fd8227c1fa9ea03d6cde0acaf0
Brian Flynn <flynnbr@tcd.ie>
2020-09-08 00:35:04 +0100

    pair selection order through fast random sampleing

--- qmla/growth_rules/genetic_algorithms/genetic_algorithm.py
+++ qmla/growth_rules/genetic_algorithms/genetic_algorithm.py
@@ -579,11 +579,18 @@ class GeneticAlgorithmQMLA():
         pair_idx = self.chrom_pair_df.index
         probabilities = self.chrom_pair_df.probability
 
-        pair_selection_order = np.random.choice(
-            a = pair_idx,
-            size = len(pair_idx), 
-            p = probabilities,
-            replace=False
+        # pair_selection_order = np.random.choice(
+        #     a = pair_idx,
+        #     size = len(pair_idx), 
+        #     p = probabilities,
+        #     replace=False
+        # )
+
+        pair_selection_order = multidimensional_shifting(
+            1, 
+            np.shape(np.where(probabilities != 0))[1], 
+            pair_idx, 
+            probabilities
         )
         self.log_print([
             "pair_selection_order: \n", repr(pair_selection_order)
@@ -757,3 +764,12 @@ class GeneticAlgorithmFullyConnectedLikewisePauliTerms(GeneticAlgorithmQMLA):
             **kwargs
         )
         
+def multidimensional_shifting(num_samples, sample_size, elements, probabilities):
+    # replicate probabilities as many times as `num_samples`
+    replicated_probabilities = np.tile(probabilities, (num_samples, 1))
+    # get random shifting numbers & scale them correctly
+    random_shifts = np.random.random(replicated_probabilities.shape)
+    random_shifts /= random_shifts.sum(axis=1)[:, np.newaxis]
+    # shift by numbers & find largest (by finding the smallest of the negative)
+    shifted_probabilities = random_shifts - replicated_probabilities
+    return np.argpartition(shifted_probabilities, sample_size, axis=1)[:, :sample_size]
