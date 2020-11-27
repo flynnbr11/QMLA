7a2251fe044903597a9706c131e5e147f517a4bd
Brian Flynn <flynnbr@tcd.ie>
2020-11-27 14:31:01 +0000

    fixed params for probe training tests

--- qmla/exploration_strategies/thesis_demos/learning_probes.py
+++ qmla/exploration_strategies/thesis_demos/learning_probes.py
@@ -31,13 +31,34 @@ class DemoProbes(
             "pauliSet_1J3_zJz_d5+pauliSet_1J4_zJz_d5+pauliSet_1J5_zJz_d5+pauliSet_2J4_zJz_d5+pauliSet_2J5_zJz_d5+pauliSet_3J4_zJz_d5+pauliSet_3J5_zJz_d5", # ising
             "pauliSet_1J2_zJz_d4+pauliSet_1J3_zJz_d4+pauliSet_2J3_xJx_d4+pauliSet_2J3_zJz_d4+pauliSet_2J4_xJx_d4+pauliSet_3J4_zJz_d4" # heisenberg
         ]
+        self.true_model_terms_params = {
+            "pauliSet_1_z_d1" : 0.2, 
+
+            "pauliSet_1_x_d1" : 0.75, 
+            "pauliSet_1_y_d1" : 0.8, 
+
+            "pauliSet_1J3_zJz_d5": 0.322,
+            "pauliSet_1J4_zJz_d5": 0.786,
+            "pauliSet_1J5_zJz_d5": 0.734,
+            "pauliSet_2J4_zJz_d5": 0.388,
+            "pauliSet_2J5_zJz_d5": 0.356,
+            "pauliSet_3J4_zJz_d5": 0.177,
+            "pauliSet_3J5_zJz_d5": 0.105,
+
+            "pauliSet_1J2_zJz_d4": 0.475,
+            "pauliSet_1J3_zJz_d4": 0.14,
+            "pauliSet_2J3_xJx_d4": 0.219,
+            "pauliSet_2J3_zJz_d4" : 0.744,
+            "pauliSet_2J4_xJx_d4": 0.108,
+            "pauliSet_3J4_zJz_d4": 0.145
+        }
         self.system_probes_generation_subroutine = qmla.shared_functionality.probe_set_generation.separable_probe_dict
 
         self._shared_true_parameters = False # test different models at each instance
         if self._shared_true_parameters:
             true_model_idx = 0
         else:
-            true_model_idx = self.qmla_id % len(self.qhl_models)  
+            true_model_idx = self.qmla_id % len(self.qhl_models) - 1 
         self.log_print(["self._shared_true_parameters = {} true_model_idx = {}".format(self._shared_true_parameters, true_model_idx)] )
 
         self.true_model = self.qhl_models[true_model_idx]
@@ -70,4 +91,20 @@ class DemoProbesPlus(
             exploration_rules=exploration_rules,
             **kwargs
         )
-        self.system_probes_generation_subroutine = qmla.shared_functionality.probe_set_generation.plus_probes_dict        
+        self.system_probes_generation_subroutine = qmla.shared_functionality.probe_set_generation.plus_probes_dict
+
+
+class DemoProbesZero(
+    DemoProbes
+):
+    def __init__(
+        self,
+        exploration_rules,
+        **kwargs
+    ):
+
+        super().__init__(
+            exploration_rules=exploration_rules,
+            **kwargs
+        )
+        self.system_probes_generation_subroutine = qmla.shared_functionality.probe_set_generation.zero_state_probes
