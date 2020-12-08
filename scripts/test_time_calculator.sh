36b1b81e4261377eebd0918b299ab0730bcd1494
Brian Flynn <flynnbr@tcd.ie>
2020-12-08 16:06:58 +0000

    updated time calculation scripts

--- scripts/test_time_calculator.sh
+++ scripts/test_time_calculator.sh
@@ -4,27 +4,18 @@ RESULTS_DIR='../DevelopmentNotebooks'
 script_name="call_time_script.sh"
 script_path="$RESULTS_DIR/$script_name"
 
-# additional_growth="-agr=IsingGenetic -agr=hubbard_square_lattice_generalised"
-additional_growth=""
+# additional_es="-agr=IsingGenetic -agr=hubbard_square_lattice_generalised"
+additional_es=""
 
-# -ggr='two_qubit_ising_rotation_hyperfine' \
-# -ggr='two_qubit_ising_rotation_hyperfine_transverse' \
-
-# -ggr='ising_1d_chain' \
-# -ggr='hubbard_square_lattice_generalised' \
-
-# growth_rule='two_qubit_ising_rotation_hyperfine_transverse'
-# growth_rule='IsingLatticeSet'
-# growth_rule='TestSimulatedNVCentre'
-growth_rule='NVCentreNQubitBath'
-num_experiments=1000
-num_particles=3000
+exploration_strategy='DemoObjectiveFunctions'
+num_experiments=500
+num_particles=2000
 
 
 python3 time_required_calculation.py \
-	-ggr=$growth_rule \
-	$additional_growth \
-	-use_agr=0 \
+	-es=$exploration_strategy \
+	$additional_es \
+	-use_aes=0 \
 	-e=$num_experiments \
 	-p=$num_particles \
 	-proc=1 \
@@ -35,6 +26,5 @@ python3 time_required_calculation.py \
 	-qhltenv="QHL_TIME" \
 	-fqhltenv="FQHL_TIME" \
 	-num_proc_env="NUM_PROC"
-
 
 echo "TIME requested $QMD_TIME" 
