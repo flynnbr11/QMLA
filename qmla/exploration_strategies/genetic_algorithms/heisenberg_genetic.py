8169fdf053146ce6d47545d13a1ad0f79711fe9e
Brian Flynn <flynnbr@tcd.ie>
2021-06-30 22:21:04 +0100

    test ES for heis

--- qmla/exploration_strategies/genetic_algorithms/heisenberg_genetic.py
+++ qmla/exploration_strategies/genetic_algorithms/heisenberg_genetic.py
@@ -7,6 +7,7 @@ import copy
 import scipy
 import time
 import pandas as pd
+import sklearn
 
 from qmla.exploration_strategies.genetic_algorithms.genetic_exploration_strategy import (
     GeneticAlgorithmQMLAFullyConnectedLikewisePauliTerms,
@@ -91,7 +92,6 @@ class HeisenbergGeneticXYZ(GeneticAlgorithmQMLAFullyConnectedLikewisePauliTerms)
         self.fraction_particles_for_bf = 0.4
         self.fraction_own_experiments_for_bf = 0.4
         self.fraction_opponents_experiments_for_bf = 0.4
-        self.iqle_mode = True
 
         # Parameter learning
         self.max_time_to_consider = 15
@@ -125,3 +125,79 @@ class TestHeisenbergGeneticXYZ(HeisenbergGeneticXYZ):
         self.initial_models = self.genetic_algorithm.random_initial_models(
             num_models=self.initial_num_models
         )
+
+class HeisenbergGeneticSingleLayer(HeisenbergGeneticXYZ):
+    def __init__(
+        self, exploration_rules, true_model=None, **kwargs  # using that set by parent
+    ):
+        true_model = "pauliSet_3J4_zJz_d5+pauliSet_4J5_zJz_d5"
+        super().__init__(
+            exploration_rules=exploration_rules, true_model=true_model, **kwargs
+        )
+
+        test_fitness_models = [
+            # TODO these models F1-score is with respect to Ising model - find equivalent for Heis
+
+            # F=0
+            "pauliSet_3J4_zJz_d5+pauliSet_4J5_zJz_d5",
+            # 'pauliSet_2J4_zJz_d5+pauliSet_3J4_zJz_d5+pauliSet_4J5_zJz_d5',
+            # 0.2 <= f < 0.3
+            "pauliSet_1J4_zJz_d5+pauliSet_1J5_zJz_d5+pauliSet_2J5_zJz_d5+pauliSet_3J4_zJz_d5",  # F=0.2
+            # 'pauliSet_1J4_zJz_d5+pauliSet_3J4_zJz_d5+pauliSet_3J5_zJz_d5',
+            # 0.3 <= f < 0.4
+            "pauliSet_2J4_zJz_d5+pauliSet_2J5_zJz_d5",
+            # 'pauliSet_1J2_zJz_d5+pauliSet_1J5_zJz_d5+pauliSet_2J4_zJz_d5+pauliSet_3J4_zJz_d5+pauliSet_3J5_zJz_d5+pauliSet_4J5_zJz_d5',
+            # 0.4 <= f < 0.5
+            "pauliSet_1J2_zJz_d5+pauliSet_1J5_zJz_d5+pauliSet_2J4_zJz_d5+pauliSet_2J5_zJz_d5+pauliSet_4J5_zJz_d5",
+            # 'pauliSet_1J4_zJz_d5+pauliSet_2J3_zJz_d5+pauliSet_2J4_zJz_d5+pauliSet_3J5_zJz_d5',
+            # 0.5 <= f < 0.6
+            "pauliSet_1J3_zJz_d5+pauliSet_1J4_zJz_d5+pauliSet_1J5_zJz_d5+pauliSet_2J4_zJz_d5+pauliSet_2J5_zJz_d5+pauliSet_3J4_zJz_d5+pauliSet_3J5_zJz_d5",
+            # 'pauliSet_1J3_zJz_d5+pauliSet_1J5_zJz_d5+pauliSet_2J4_zJz_d5+pauliSet_2J5_zJz_d5+pauliSet_3J4_zJz_d5+pauliSet_3J5_zJz_d5',
+            # 0.6 <= f < 0.7
+            "pauliSet_1J3_zJz_d5+pauliSet_1J4_zJz_d5+pauliSet_1J5_zJz_d5+pauliSet_2J5_zJz_d5+pauliSet_3J5_zJz_d5",
+            # 'pauliSet_1J3_zJz_d5+pauliSet_2J4_zJz_d5+pauliSet_2J5_zJz_d5+pauliSet_3J5_zJz_d5',
+            # 0.7 <= f < 0.8
+            "pauliSet_1J2_zJz_d5+pauliSet_1J3_zJz_d5+pauliSet_1J4_zJz_d5+pauliSet_1J5_zJz_d5+pauliSet_2J3_zJz_d5+pauliSet_2J4_zJz_d5+pauliSet_2J5_zJz_d5+pauliSet_3J5_zJz_d5+pauliSet_4J5_zJz_d5",
+            # 'pauliSet_1J2_zJz_d5+pauliSet_1J3_zJz_d5+pauliSet_1J5_zJz_d5+pauliSet_2J3_zJz_d5+pauliSet_2J5_zJz_d5+pauliSet_4J5_zJz_d5',
+            # 0.8 <= f < 0.9
+            "pauliSet_1J2_zJz_d5+pauliSet_1J3_zJz_d5+pauliSet_2J4_zJz_d5+pauliSet_2J5_zJz_d5+pauliSet_3J5_zJz_d5",  # F=0.8
+            # 'pauliSet_1J2_zJz_d5+pauliSet_1J3_zJz_d5+pauliSet_2J3_zJz_d5+pauliSet_2J4_zJz_d5+pauliSet_2J5_zJz_d5+pauliSet_3J4_zJz_d5+pauliSet_3J5_zJz_d5',
+            # 0.9 <= f < 1
+            "pauliSet_1J2_zJz_d5+pauliSet_2J3_zJz_d5+pauliSet_2J5_zJz_d5+pauliSet_3J5_zJz_d5",
+            # F = 1
+            "pauliSet_1J2_zJz_d5+pauliSet_1J3_zJz_d5+pauliSet_2J3_zJz_d5+pauliSet_2J5_zJz_d5+pauliSet_3J5_zJz_d5",  # F=1
+        ]
+        # 10 models per layer, fully connected -> 45 comparisons using optimal_graph; 90 using all
+
+        self.initial_models = list(
+            np.random.choice(
+                test_fitness_models, len(test_fitness_models), replace=False
+            )
+        )
+        if self.true_model not in self.initial_models:
+            rand_idx = self.initial_models.index(np.random.choice(self.initial_models))
+            self.initial_models[rand_idx] = self.true_model
+
+        self.branch_comparison_strategy = "optimal_graph" # 'all'
+        self.tree_completed_initially = True
+        if self.tree_completed_initially:
+            self.max_spawn_depth = 1
+        self.initial_num_models = len(self.initial_models)
+        self.max_num_models_by_shape = {
+            self.num_sites: (len(self.initial_models) * self.max_spawn_depth) / 8,
+            "other": 0,
+        }
+
+        self.num_processes_to_parallelise_over = 16
+        self.timing_insurance_factor = 0.75
+
+class HeisenbergGeneticTest(HeisenbergGeneticXYZ):
+    def __init__(
+        self, exploration_rules, true_model=None, **kwargs  # using that set by parent
+    ):
+        true_model = "pauliSet_1J3_zJz_d5+pauliSet_1J4_zJz_d5+pauliSet_1J5_zJz_d5+pauliSet_2J4_zJz_d5+pauliSet_2J5_zJz_d5+pauliSet_3J4_zJz_d5"
+        super().__init__(
+            exploration_rules=exploration_rules, true_model=true_model, **kwargs
+        )
+        self.iqle_mode = True
+
